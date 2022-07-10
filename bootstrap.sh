ec0ed80a7907dd34eed16f69a971926615e650a9
Mark Cornick <mark@markcornick.com>
2022-07-10 01:08:21 -0400

    Simplify vim local config

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,6 @@ esac
 . "\$HOME/.bashrc.local"
 E0F
 cat << E0F > "$HOME/.vimrc"
-source ~/.vimrc.local.before
 filetype plugin indent on
 syntax enable
 nnoremap <silent> <C-L> :nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR><C-L>
@@ -60,9 +59,9 @@ set ttimeout
 set ttimeoutlen=100
 set viewoptions-=options
 set wildmenu
-source ~/.vimrc.local.after
+source ~/.vimrc.local
 E0F
-touch "$HOME/.vimrc.local.before" "$HOME/.vimrc.local.after"
+touch "$HOME/.vimrc.local"
 cat << E0F > "$HOME/.zshrc"
 PS1="%n@%m:%~%# "
 HISTFILE=\$HOME/.zsh_history
